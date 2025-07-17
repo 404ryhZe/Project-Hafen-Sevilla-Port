5f50559639a48546cb695a61dc32f217a28e041c
404ryhZe <mail@emilflossmann.de>
2025-07-17 14:27:01 +0200

    git -M "1707v2"

--- Main.cpp
+++ Main.cpp
@@ -1,26 +1,28 @@
-﻿//
-// Created by emilf on 07.07.2025.
+﻿// Created by emilf on 07.07.2025.
 //
-#include "Port.h"
-#include <iostream>
-#include <ctime>
+#include "Port.h"              // Einbindung der Header-Datei für die Port-Klasse
+#include <iostream>            // Für Ein- und Ausgabe
+#include <ctime>               // Für Zeitfunktionen (z. B. zur Initialisierung des Zufallsgenerators)
 
 int main()
 {
-    srand(static_cast<unsigned>(time(0))); // Initialisiert Zufallsgenerator
+    // Initialisiert den Zufallsgenerator mit der aktuellen Zeit als Seed
+    srand(static_cast<unsigned>(time(0)));
 
+    // Erstellt ein Port-Objekt
     Port port;
 
-    // Beispielhafte Schiffe hinzufügen
+    // Fügt dem Hafen drei beispielhafte Schiffe mit zufälliger Ankunftszeit und Liegezeit hinzu
     port.addShip(Ship("Poseidon", "Container", rand() % 25 + 1, rand() % 6 + 1));
-    port.addShip(Ship("Neptune", "Gas", rand() % 25 + 1,rand() % 6 + 1));
+    port.addShip(Ship("Neptune", "Gas", rand() % 25 + 1, rand() % 6 + 1));
     port.addShip(Ship("Athena", "Schuettgut", rand() % 25 + 1, rand() % 6 + 1));
 
-    // Simulation für 24 Stunden
+    // Simuliert den Ablauf von 24 Stunden im Hafen
     for (int i = 0; i < 24; ++i) {
-        port.advanceTime();
+        port.advanceTime(); // Fortschreiten der Zeit um eine Stunde
     }
 
+    // Gibt eine Abschlussmeldung aus
     std::cout << "\n--- Simulation beendet ---\n";
     return 0;
 }
